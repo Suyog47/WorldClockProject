0e1858009cb27fab8f122d8924f5c6e3ba2fa649
Suyog47 <suyogamin11@gmail.com>
2020-12-22 19:41:19 +0530

    updated app launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:worldclockproject/Activities/home.dart';
 import 'package:worldclockproject/Activities/location.dart';
 import 'package:worldclockproject/Activities/loading.dart';
 
-void main() => runApp(MaterialApp(
-  routes: {
-    "/": (context) => Loading(),
-    "/home": (context) => Home(),
-    "/location": (context) => Location(),
-  },
-));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown,DeviceOrientation.portraitUp]);
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (context) => Loading(),
+        "/home": (context) => Home(),
+        "/location": (context) => Location(),
+      },
+    );
+  }
+}
